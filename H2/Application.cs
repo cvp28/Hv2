d10af56a30fb008cc4e0f774cda8b4cf353a5414
cvp28 <carson.vp28@gmail.com>
2024-04-05 15:23:31 -0700

    Initial Commit

---
+++ H2/Application.cs
@@ -0,0 +1,21 @@
+﻿using Cosmo;
+
+namespace H2;
+
+public class Application
+{
+	private Renderer CosmoRenderer;
+	
+	internal Application()
+	{
+		CosmoRenderer = new();
+	}
+	
+	/// <summary>
+	/// Blocking call. Executes the current application.
+	/// </summary>
+	public void Run()
+	{
+		
+	}
+}
---
+++ H2/H2.cs
@@ -0,0 +1,9 @@
+﻿namespace H2;
+
+public static class H2
+{
+	public static Application CreateApp()
+	{
+		return new();
+	}
+}
