ebd1ef8790a19ea6d529f0d39f1e7007ab24a542
cvp28 <carson.vp28@gmail.com>
2024-04-08 09:15:58 -0700

    H2 1.1.1

--- H2/Application.cs
+++ H2/Application.cs
@@ -1,4 +1,6 @@
-﻿using Cosmo;
+﻿using System.Diagnostics;
+
+using Cosmo;
 
 namespace H2;
 
@@ -6,9 +8,32 @@ public class Application
 {
 	private Renderer CosmoRenderer;
 	
-	internal Application()
+	public Action<Renderer> RenderAction;
+	
+	private bool Running = false;
+	
+	private System.Timers.Timer FrameTimer;
+	private int LastFPS;
+	public int CurrentFPS { get; private set; }
+	
+	
+	public Application()
 	{
 		CosmoRenderer = new();
+
+		FrameTimer = new()
+		{
+			Interval = 1000
+		};
+		
+		FrameTimer.Elapsed += (obj, args) => 
+		{
+			LastFPS = CurrentFPS;
+			CurrentFPS = 0;
+		};
+
+		LastFPS = 0;
+		CurrentFPS = 0;
 	}
 	
 	/// <summary>
@@ -16,6 +41,19 @@ public class Application
 	/// </summary>
 	public void Run()
 	{
+		FrameTimer.Start();
 		
+		while (Running)
+		{
+			RenderAction(CosmoRenderer);
+			CosmoRenderer.Flush();
+			CurrentFPS++;
+		}
+	}
+	
+	public void SignalExit()
+	{
+		Running = false;
+		FrameTimer.Stop();
 	}
 }
