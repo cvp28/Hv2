57dc2b64cfd40886be02e6c30cda0ad63fef060b
cvp28 <carson.vp28@gmail.com>
2024-06-07 15:57:03 -0700

    Progress 06-07-2024

---
+++ Hv2/Widgets/ScrollableMenu/ScrollableMenu.cs
@@ -0,0 +1,266 @@
+﻿using Cosmo;
+
+namespace Hv2UI;
+
+public class ScrollableMenu : Widget
+{
+	public int X { get; set; }
+	public int Y { get; set; }
+	
+	public int Height { get; set; }
+	
+	// Called whenever the user selects an action
+	public Action<int, string> OnSubmit;
+	
+	private int ScrollY = 0; // Index that tracks where in the menu we have scrolled to
+	private int ScrollYMax => Options.Count <= Height ? 0 : Options.Count - Height;
+	
+	private List<MenuOption> Options;
+	
+	public MenuOption this[int Index]
+	{
+		get
+		{
+			if (!IsValidIndex(Index))
+				return null;
+
+			return Options[Index];
+		}
+
+		set
+		{
+			if (!IsValidIndex(Index))
+				return;
+
+			Options[Index] = value;
+		}
+	}
+
+	public int OptionCount => Options.Count;
+
+	public MenuStyle SelectedOptionStyle { get; set; }
+	public Alignment TextAlignment { get; set; }
+
+	/// <summary>
+	/// Determines if the options will always be drawn styled regardless of if the menu is focused
+	/// </summary>
+	public bool DoStyle { get; set; }
+
+	public int SelectedOption { get; internal set; }
+	
+	/// <summary>
+	/// Called whenever the user selects an option with the arrow keys. Receives current index and option text.
+	/// </summary>
+	public Action<int, string> OnSelectionChange { get; set; }
+	
+	public ScrollableMenu(int X, int Y, int Height)
+	{
+		this.X = X;
+		this.Y = Y;
+		this.Height = Height;
+		
+		SelectedOption = 0;
+		
+		DoStyle = true;
+		SelectedOptionStyle = MenuStyle.Highlighted;
+
+		Options = new();
+	}
+	
+	public override void Draw(Renderer r)
+	{
+		switch (TextAlignment)
+		{
+			case Alignment.Center:
+				DrawCenterAligned(r);
+				break;
+
+			default:
+			case Alignment.Left:
+				DrawLeftAligned(r);
+				break;
+		}
+		
+		// Fancy ass unicode arrows ↑ ↓
+		if (!AtTop)
+		{
+			//	RenderContext.VTSetCursorPosition(State.X, State.Y);
+			//	RenderContext.VTDrawChar('↑');
+			
+			r.WriteAt(X, Y, "↑");
+		}
+		
+		if (!AtBottom)
+		{
+			//	RenderContext.VTSetCursorPosition(State.X, State.Y + State.Height - 1);
+			//	RenderContext.VTDrawChar('↓');
+			
+			r.WriteAt(X, Y + Height - 1, "↓");
+		}
+	}
+	
+	void DrawLeftAligned(Renderer r)
+		{
+			int CurrentYOff = 0;
+			int DrawCount = Options.Count > Height ? Height : Options.Count;
+			
+			for (int i = ScrollY; i < ScrollY + DrawCount; i++)
+			{
+				if (i == SelectedOption && DoStyle)
+					DrawStyledOption(X + 1, Y + CurrentYOff, Options[i], r);
+				else
+					r.WriteAt(X + 1, Y + CurrentYOff, Options[i].Text, Options[i].TextForeground, Options[i].TextBackground, StyleCode.None);
+				
+				CurrentYOff++;
+			}
+		}
+
+		void DrawCenterAligned(Renderer r)
+		{
+			int CurrentYOff = 0;
+			int LongestOptionLength = Options.Max(op => op.Text.Length);
+			int DrawCount = Options.Count > Height ? Height : Options.Count;
+
+			for (int i = ScrollY; i < ScrollY + DrawCount; i++)
+			{
+				int CurrentX = X + (LongestOptionLength / 2) - (Options[i].Text.Length / 2) + 1;
+
+				if (i == SelectedOption && DoStyle)
+					DrawStyledOption(CurrentX, Y + CurrentYOff, Options[i], r);
+				else
+					r.WriteAt(CurrentX, Y + CurrentYOff, Options[i].Text, Options[i].TextForeground, Options[i].TextBackground, StyleCode.None);
+				
+				CurrentYOff++;
+			}
+		}
+
+		void DrawStyledOption(int X, int Y, MenuOption Option, Renderer r)
+		{
+			switch (SelectedOptionStyle)
+			{
+				case MenuStyle.Arrow:
+					{
+						//	RenderContext.VTSetCursorPosition(X, Y);
+						//	RenderContext.VTEnterColorContext(Option.TextForeground, Option.TextBackground, delegate ()
+						//	{
+						//		RenderContext.VTDrawText(Option.Text);
+						//		RenderContext.VTDrawChar(' ');
+						//		RenderContext.VTDrawChar('<');
+						//	});
+						
+						r.WriteAt(X, Y, $"{Option.Text} <", Option.TextForeground, Option.TextBackground, StyleCode.None);
+						break;
+					}
+
+				case MenuStyle.Highlighted:
+					{
+						//	RenderContext.VTSetCursorPosition(X, Y);
+						//	RenderContext.VTInvert();
+						//	RenderContext.VTDrawText(Option.Text);
+						//	RenderContext.VTRevert();
+						
+						r.WriteAt(X, Y, Option.Text, Option.TextForeground, Option.TextBackground, StyleCode.Inverted);
+						break;
+					}
+			}
+		}
+	
+	public override void OnInput(ConsoleKeyInfo cki)
+	{
+		switch (cki.Key)
+		{
+			case ConsoleKey.UpArrow:
+				if (SelectedOption == 0)
+				{
+					SelectedOption = Options.Count - 1;
+					ScrollY = ScrollYMax;
+				}
+				else
+				{
+					SelectedOption--;
+					
+					if (SelectedOption < ScrollY)
+						ScrollY = SelectedOption;
+				}
+				
+				if (OnSelectionChange is not null)
+					OnSelectionChange(SelectedOption, this[SelectedOption].Text);
+				break;
+
+			case ConsoleKey.DownArrow:
+				if (SelectedOption == Options.Count - 1)
+				{
+					SelectedOption = 0;
+					ScrollY = 0;
+				}
+				else
+				{
+					SelectedOption++;
+					
+					if (SelectedOption - ScrollY > Height - 1)
+						ScrollY++;
+				}
+				
+				if (OnSelectionChange is not null)
+					OnSelectionChange(SelectedOption, this[SelectedOption].Text);
+				break;
+			
+			case ConsoleKey.Enter:
+				this[SelectedOption].Action();
+				if (OnSubmit is not null) OnSubmit(SelectedOption, this[SelectedOption].Text);
+				break;
+		}
+	}
+	
+	private bool IsValidIndex(int Index) => Index >= 0 && Index < Options.Count;
+	
+	private bool AtTop => ScrollY == 0;
+	
+	private bool AtBottom => ScrollY == ScrollYMax;
+	
+	public void AddOption(string Option, Action Action) => AddOption(Option, Action, Color24.White, Color24.Black);
+	
+	public void AddOption(string Option, Action Action, Color24 Foreground, Color24 Background)
+	{
+		if (Option is null || Action is null)
+			return;
+
+		var NewOption = new MenuOption()
+		{
+			Index = Options.Count,
+			
+			Text = Option,
+			Action = Action,
+			
+			TextForeground = Foreground,
+			TextBackground = Background
+		};
+
+		Options.Add(NewOption);
+	}
+
+	public void RemoveAllOptions()
+	{
+		SelectedOption = 0;
+		ScrollY = 0;
+		Options.Clear();
+	}
+
+	public void CenterTo(Dimensions d, int XOff = 0, int YOff = 0)
+	{
+		if (Options.Count == 0)
+			return;
+
+		int LongestOptionLength = Options.Max(op => op.Text.Length);
+
+		X = d.HorizontalCenter - (LongestOptionLength / 2) + XOff;
+		Y = d.VerticalCenter - (int) Math.Ceiling( OptionCount / 2.0f ) + YOff;
+
+		if (X < 0)
+			X = 0;
+
+		if (Y < 0)
+			Y = 0;
+	}
+	
+}
---
+++ Hv2/Widgets/SearchField/SearchField.cs
@@ -0,0 +1,172 @@
+﻿using Cosmo;
+
+namespace Hv2UI;
+
+public class SearchField<T> : Widget
+{
+	public int X { get; set; }
+	public int Y { get; set; }
+	
+	public int MenuHeight { get => Menu.Height; set => Menu.Height = value; }
+	
+	public Func<string, Task<IEnumerable<(string, T)>>> SearchAction;
+	
+	private InputField Input;
+	private ScrollableMenu Menu;
+	
+	private IEnumerable<(string Text, T Result)> CurrentResults;
+	
+	private string CurrentMessage = string.Empty;
+	
+	// If true, focus input field
+	// Else, focus menu
+	private bool InputHasFocus = true;
+	
+	private Task SearchTask;
+	
+	private bool Searching => SearchTask is not null && !SearchTask.IsCompleted;
+	private bool HasResults => Menu.OptionCount > 0;
+	private bool ResultsReady => !Searching && HasResults;
+	
+	public SearchField(int X, int Y, string Prompt, int Height, Func<string, Task<IEnumerable<(string, T)>>> SearchAction)
+	{
+		this.X = X;
+		this.Y = Y;
+		
+		this.SearchAction = SearchAction;
+		
+		Input = new()
+		{
+			X = X,
+			Y = Y,
+			Prompt = Prompt,
+			CursorVisible = true,
+			ClearOnSubmit = false
+		};
+		
+		Menu = new(X + Input.Prompt.Length - 1, Y + 1, Height)
+		{
+			DoStyle = false
+		};
+		
+		Input.OnInputReady = OnSubmit;
+	}
+	
+	private void OnSubmit(string Input)
+	{
+		if (Searching)
+			return;
+			
+		CurrentMessage = "Searching...";
+		Menu.RemoveAllOptions();
+		
+		SearchTask = Task.Run(async delegate
+		{
+			CurrentResults = await SearchAction(Input);		// Run the client-defined search action and retrieve the results
+			
+			if (CurrentResults is null || !CurrentResults.Any())
+			{
+				ResetState(false);
+				return;
+			}
+			
+			// Populate menu
+			foreach (var Result in CurrentResults)
+				Menu.AddOption(Result.Text, delegate {});
+			
+			CurrentMessage = string.Empty;
+		});
+	}
+	
+	private bool OnChar(string Input)
+	{
+		if (Searching)
+			return false;
+		
+		CurrentMessage = "Searching...";
+		Menu.RemoveAllOptions();
+		
+		SearchTask = Task.Run(async delegate
+		{
+			CurrentResults = await SearchAction(Input);		// Run the client-defined search action and retrieve the results
+			
+			if (CurrentResults is null || !CurrentResults.Any())
+			{
+				ResetState(false);
+				return;
+			}
+			
+			// Populate menu
+			foreach (var Result in CurrentResults)
+				Menu.AddOption(Result.Text, delegate {});
+			
+			CurrentMessage = string.Empty;
+		});
+		
+		return false;
+	}
+	
+	public void EnableImmediateRefresh()
+	{
+		Input.OnCharInput = OnChar;
+		Input.OnInputReady = null;
+	}
+	
+	public void DisableImmediateRefresh()
+	{
+		Input.OnCharInput = null;
+		Input.OnInputReady = OnSubmit;
+	}
+	
+	// Resets the state (no shit)
+	// Only hard resets clear the buffer
+	private void ResetState(bool Hard)
+	{
+		if (Hard) Input.Clear();
+		Menu.RemoveAllOptions();
+		
+		// If the task is already running, then undo its work when it finishes
+		if (Searching) SearchTask.ContinueWith((t) => Menu.RemoveAllOptions());
+		
+		InputHasFocus = true;
+		
+		Input.CursorVisible = true;
+		Menu.DoStyle = false;
+		
+		CurrentMessage = string.Empty;
+	}
+	
+	public override void OnInput(ConsoleKeyInfo cki)
+	{
+		if (cki.Key == ConsoleKey.Tab)
+		{
+			// If we're currently searching or there are no results to select, ignore this key
+			if (Searching || !HasResults) return;
+			
+			InputHasFocus = !InputHasFocus;
+		}
+		
+		if (cki.Key == ConsoleKey.Escape)
+		{
+			if (InputHasFocus) ResetState(true);
+		}
+		
+		
+	}
+
+	public override void Draw(Renderer r)
+	{
+		Input.CursorVisible = InputHasFocus;
+		Menu.DoStyle = !InputHasFocus;
+		
+		if (InputHasFocus)
+			Hv2.FocusedWidget = Input;
+		else
+			Hv2.FocusedWidget = Menu;
+		
+		Input.Draw(r);
+		Menu.Draw(r);
+	}
+	
+	
+}
