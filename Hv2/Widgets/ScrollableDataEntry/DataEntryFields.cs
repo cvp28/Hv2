9f9cb4a2dafe0b7d208954634b07f7d43ea3e7c7
cvp28 <carson.vp28@gmail.com>
2024-11-09 10:13:50 -0800

    Progress 11/8/2024
    Continuing to work on the ScrollableDataEntry widget and adding different types of fields. This commit also doesn't compile. Woops.

---
+++ Hv2/Widgets/ScrollableDataEntry/DataEntryFields.cs
@@ -0,0 +1,89 @@
+﻿
+using Cosmo;
+
+namespace Hv2UI;
+
+public class DataEntryField
+{
+    public string ID;
+
+    public string Text;
+    public Color24 TextForeground;
+    public Color24 TextBackground;
+
+    public DataEntryField()
+    {
+        TextForeground = Color24.White;
+        TextBackground = Color24.Black;
+    }
+
+    /// <summary>
+    /// Determines if the field should be displayed
+    /// </summary>
+    public Func<bool> VisibilityRule = () => true;
+}
+
+public class TextField : DataEntryField
+{
+    public TextField(string Text)
+    {
+        base.Text = Text;
+
+        defInputField = new()
+        {
+            X = 0,
+            Y = 0,
+
+            CursorVisible = false,
+
+            HistoryEnabled = false,
+            HighlightingEnabled = false
+        };
+    }
+
+    internal InputField defInputField;
+
+    public string Value
+    {
+        get => defInputField.Buffer.ToString();
+
+        set
+        {
+            if (value is null)
+                return;
+
+            defInputField.Buffer.Clear();
+            defInputField.Buffer.Append(value);
+        }
+    }
+}
+
+public class BooleanCheckboxField : DataEntryField
+{
+    public bool Checked;
+
+    public BooleanCheckboxField(string Text, bool Checked = true)
+    {
+        base.Text = Text;
+        this.Checked = Checked;
+    }
+}
+
+public class BooleanOptionField : DataEntryField
+{
+    public string Option1;
+    public string Option2;
+
+    /// <summary>
+    /// true for Option1, false for Option2
+    /// </summary>
+    public bool Selected;
+
+    public BooleanOptionField(string Text, string Option1, string Option2, bool Selected = true)
+    {
+        base.Text = Text;
+        this.Option1 = Option1;
+        this.Option2 = Option2;
+        this.Selected = Selected;
+    }
+}
--- Hv2/Widgets/ScrollableDataEntry/ScrollableDataEntry.cs
+++ Hv2/Widgets/ScrollableDataEntry/ScrollableDataEntry.cs
@@ -1,7 +1,5 @@
 ﻿using Cosmo;
-using System.Diagnostics;
-using System.Text;
-using System.Timers;
+using System.Security.Principal;
 
 namespace Hv2UI;
 
@@ -11,12 +9,6 @@ public class ScrollableDataEntry : Widget
 	public int Y { get; set; }
 	
 	public int Height { get; set; }
-	
-	// Called whenever the user selects an action
-	public Action<int, string> OnSubmit;
-
-	private Task<Maybe<MenuOption>> SubmitTask = null;
-	private MenuOption SubmitResult = null;
 	
 	private int ScrollY = 0; // Index that tracks where in the menu we have scrolled to
 	private int ScrollYMax => Fields.Count <= Height ? 0 : Fields.Count - Height;
@@ -42,7 +34,23 @@ public class ScrollableDataEntry : Widget
 		}
 	}
 
-	public DataEntryField this[string ID] => Fields.First(f => f.ID == ID);
+	public DataEntryField this[string ID]
+	{
+		get => Fields.FirstOrDefault(f => f.ID == ID);
+
+		set
+		{
+			var test = this[ID];
+
+			if (test is null)
+			{
+				value.ID = ID;
+				Fields.Add(value);
+			}
+			else
+				Fields[Fields.IndexOf(test)] = value;
+		}
+	}
 
 	public int FieldCount => Fields.Count;
 
@@ -67,190 +75,174 @@ public class ScrollableDataEntry : Widget
     public override void Draw(Renderer r)
 	{
         int CurrentYOff = 0;
-        int DrawCount = Fields.Count > Height ? Height : Fields.Count;
+
+		var VisibleFields = Fields.Where(f => f.VisibilityRule()).ToArray();
+        int DrawCount = VisibleFields.Length > Height ? Height : VisibleFields.Length;
 
         for (int i = ScrollY; i < ScrollY + DrawCount; i++)
         {
-			if (!Fields[i].VisibilityRule())
-				continue;
+			//	if (!Fields[i].VisibilityRule())
+			//		continue;
 
-            r.WriteAt(X + 1, Y + CurrentYOff, Fields[i].Text, Fields[i].TextForeground, Fields[i].TextBackground, StyleCode.None);
+            r.WriteAt(X + 1, Y + CurrentYOff, VisibleFields[i].Text, VisibleFields[i].TextForeground, VisibleFields[i].TextBackground, StyleCode.None);
 
-			Fields[i].defInputField.CursorVisible = i == SelectedFieldIndex;
-			
-			Fields[i].defInputField.X = X + Fields[i].Text.Length + 1;
-            Fields[i].defInputField.Y = Y + CurrentYOff;
-            Fields[i].defInputField.Draw(r);
+			// Field-specific rendering
+
+			switch (VisibleFields[i])
+			{
+				case TextField tf:
+                    tf.defInputField.CursorVisible = VisibleFields[i].ID == Fields[SelectedFieldIndex].ID;
+					
+                    tf.defInputField.X = X + VisibleFields[i].Text.Length + 1;
+                    tf.defInputField.Y = Y + CurrentYOff;
+                    tf.defInputField.Draw(r);
+                    break;
+
+				case BooleanCheckboxField bcf:
+					r.WriteAt(
+						X + VisibleFields[i].Text.Length + 1,
+						Y + CurrentYOff,
+
+						bcf.Checked ? "" : " ",
+
+						Color24.White,
+						Color24.Black,
+
+						bcf == CurrentlySelectedField && !bcf.Checked ? StyleCode.Inverted : StyleCode.None
+					);
+					break;
+
+				case BooleanOptionField bof:
+					r.WriteAt()
+
+					r.WriteAt(
+						X + VisibleFields[i].Text.Length + 1,
+						Y + CurrentYOff,
+
+						$"{bof.Option1}  {bof.Option2}",
+
+						Color24.White,
+						Color24.Black,
+
+						bof == CurrentlySelectedField && !
+						)
+					break;
+			}
 
             CurrentYOff++;
         }
 
         // Fancy ass unicode arrows ↑ ↓
         if (!AtTop)
-		{
-			//	RenderContext.VTSetCursorPosition(State.X, State.Y);
-			//	RenderContext.VTDrawChar('↑');
-			
 			r.WriteAt(X, Y, "↑");
-		}
 		
 		if (!AtBottom)
-		{
-			//	RenderContext.VTSetCursorPosition(State.X, State.Y + State.Height - 1);
-			//	RenderContext.VTDrawChar('↓');
-			
 			r.WriteAt(X, Y + Height - 1, "↓");
-		}
 	}
-
-	private bool CancelGetResult = false;
-
-	//	public async Task<Maybe<MenuOption>> GetResultAsync()
-    //	{
-	//		if (SubmitTask is not null)
-	//			return Maybe<MenuOption>.Fail();
-	//	
-	//		SubmitTask = Task.Run(delegate
-    //	    {
-	//			while (SubmitResult is null)
-	//			{
-	//				if (CancelGetResult)
-	//				{
-	//					CancelGetResult = false;
-	//					SubmitResult = null;
-	//					SubmitTask = null;
-	//					return Maybe<MenuOption>.Fail();
-	//				}
-	//	
-	//				Thread.Sleep(50);
-	//			}
-	//	
-    //	        var temp = SubmitResult;
-	//	
-    //	        SubmitResult = null;
-    //	        SubmitTask = null;
-	//	
-	//			return Maybe<MenuOption>.Success(temp);
-    //	    });
-	//	
-	//		return await SubmitTask;
-	//	}
 	
 	public override void OnInput(ConsoleKeyInfo cki)
 	{
 		switch (cki.Key)
 		{
 			case ConsoleKey.UpArrow:
-				if (SelectedFieldIndex == 0)
+				do
 				{
-					SelectedFieldIndex = Fields.Count - 1;
-					ScrollY = ScrollYMax;
-				}
-				else
-				{
-					SelectedFieldIndex--;
-					
-					if (SelectedFieldIndex < ScrollY)
-						ScrollY = SelectedFieldIndex;
-				}
-				
-				if (OnSelectionChange is not null)
-					OnSelectionChange(Fields[SelectedFieldIndex].ID, Fields[SelectedFieldIndex]);
+					if (SelectedFieldIndex == 0)
+					{
+						SelectedFieldIndex = Fields.Count - 1;
+						ScrollY = ScrollYMax;
+					}
+					else
+					{
+						SelectedFieldIndex--;
 
-				Fields[SelectedFieldIndex].defInputField.EnsureCursorVisible();
+						if (SelectedFieldIndex < ScrollY)
+							ScrollY = SelectedFieldIndex;
+					}
+				}
+				while (!CurrentlySelectedField.VisibilityRule());
+
+				if (OnSelectionChange is not null)
+					OnSelectionChange(CurrentlySelectedField.ID, CurrentlySelectedField);
+
+                // Field-specific actions
+
+                switch (CurrentlySelectedField)
+                {
+                    case TextField tf:
+                        tf.defInputField.EnsureCursorVisible();
+                        break;
+                }
                 break;
 
 			case ConsoleKey.DownArrow:
-				if (SelectedFieldIndex == Fields.Count - 1)
+				do
 				{
-					SelectedFieldIndex = 0;
-					ScrollY = 0;
-				}
-				else
-				{
-					SelectedFieldIndex++;
-					
-					if (SelectedFieldIndex - ScrollY > Height - 1)
-						ScrollY++;
-				}
-				
-				if (OnSelectionChange is not null)
-					OnSelectionChange(Fields[SelectedFieldIndex].ID, Fields[SelectedFieldIndex]);
+					if (SelectedFieldIndex == Fields.Count - 1)
+					{
+						SelectedFieldIndex = 0;
+						ScrollY = 0;
+					}
+					else
+					{
+						SelectedFieldIndex++;
 
-                Fields[SelectedFieldIndex].defInputField.EnsureCursorVisible();
+						if (SelectedFieldIndex - ScrollY > Height - 1)
+							ScrollY++;
+					}
+				}
+				while (!CurrentlySelectedField.VisibilityRule());
+
+                if (OnSelectionChange is not null)
+					OnSelectionChange(CurrentlySelectedField.ID, CurrentlySelectedField);
+
+				// Field-specific actions
+
+				switch (CurrentlySelectedField)
+				{
+					case TextField tf:
+						tf.defInputField.EnsureCursorVisible();
+						break;
+				}
+
                 break;
-			
-			case ConsoleKey.Enter:
-				//	this[SelectedFieldIndex].Action();
-				//	
-				//	if (OnSubmit is not null) OnSubmit(SelectedFieldIndex, this[SelectedFieldIndex].Text);
-				//	if (SubmitTask is not null) SubmitResult = this[SelectedFieldIndex];
-				break;
-
-			case ConsoleKey.Escape:
-				if (SubmitTask is not null) CancelGetResult = true;
-				break;
 
             default:
-				Fields[SelectedFieldIndex].defInputField.OnInput(cki);
+				// Field-specific input
+
+				switch (CurrentlySelectedField)
+				{
+					case TextField tf:
+						tf.defInputField.OnInput(cki);
+						break;
+
+					case BooleanCheckboxField bcf:
+						if (cki.Key == ConsoleKey.Enter || cki.Key == ConsoleKey.Spacebar)
+							bcf.Checked = !bcf.Checked;
+						break;
+				}
+
 				break;
         }
 	}
 
 	private bool IsValidIndex(int Index) => Index >= 0 && Index < Fields.Count;
-	
+
+	public DataEntryField CurrentlySelectedField => Fields[SelectedFieldIndex];
+
 	private bool AtTop => ScrollY == 0;
 	
 	private bool AtBottom => ScrollY == ScrollYMax;
 
-	//	public void AddOption(string Option, Action Action) => AddOption(Option, Action, Color24.White, Color24.Black);
-	//	
-	//	public void AddOption(string Option, Action Action, Color24 Foreground, Color24 Background)
-	//	{
-	//		if (Option is null || Action is null)
-	//			return;
-	//	
-	//		var NewOption = new MenuOption()
-	//		{
-	//			Index = Fields.Count,
-	//			
-	//			Text = Option,
-	//			Action = Action,
-	//			
-	//			TextForeground = Foreground,
-	//			TextBackground = Background
-	//		};
-	//	
-	//		Fields.Add(NewOption);
-	//	}
-
-	public void AddField(string ID, DataEntryFieldType Type, string Text) => AddField(ID, Type, Text, Color24.White, Color24.Black, () => true);
-	public void AddField(string ID, DataEntryFieldType Type, string Text, Color24 Foreground, Color24 Background) => AddField(ID, Type, Text, Foreground, Background, () => true);
-
-    public void AddField(string ID, DataEntryFieldType Type, string Text, Color24 Foreground, Color24 Background, Func<bool> VisibilityRule)
+    public void AddField(string Text, string ID, DataEntryField Field)
 	{
-		if (Fields.Any(f => f.ID == ID))
+		if (Fields.Any(f => f.ID == Field.ID))
 			return;
 
-		Fields.Add(new()
-		{
-			ID = ID,
-			Type = Type,
+		Field.Text = Text;
+		Field.ID = ID;
 
-			Text = Text,
-
-			TextForeground = Foreground,
-			TextBackground = Background,
-
-			defInputField = new()
-			{
-				X = X,
-				Y = Y,
-				CursorVisible = false
-			},
-
-			VisibilityRule = VisibilityRule
-		});
+		Fields.Add(Field);
 	}
 
 	public void RemoveAllOptions()
